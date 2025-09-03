209103658c68db1934669dbd182d20d1bc76dccf
Nithya0402 <106722982+Nithya0402@users.noreply.github.com>
2025-09-03 15:57:03 +0530

    this is my tcfsample

---
+++ nithya.java/src/basicjava/TcfSample.java
@@ -0,0 +1,17 @@
+package basicjava;
+
+public class TcfSample {
+
+    public void cts() {
+        System.out.println("Poojitha is selected in cts");
+    }
+
+    // main method = program entry point
+    public static void main(String[] args) {
+        // Create an object of TcfSample
+        TcfSample obj = new TcfSample();
+
+        // Call the method
+        obj.cts();
+    }
+}
